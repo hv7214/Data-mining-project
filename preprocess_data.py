ca5b1f43eb53ad47a414efd0eb70d7b30d81ee95
Harshit Verma <hverma1@cs.iitr.ac.in>
2022-03-27 15:53:07 +0530

    fix rows drop

--- preprocess_data.py
+++ preprocess_data.py
@@ -9,7 +9,8 @@ to_drop = bakery[bakery.Item.isin(NAs)]
 print(f"There are {len(to_drop)} rows in the dataset having Item column as N/A")
 print("Removing these rows...\n")
 
-bakery.drop(to_drop.index)
+bakery.drop(to_drop.index, inplace=True)
 
 print("Saving the preprocessed data...\n")
+print(f"Total rows after removing noise {len(bakery)}\n")
 bakery.to_csv('preprocessed_BreadBasket_DMS.csv', index=False)
